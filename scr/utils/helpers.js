ecf7c056d447f66d703bdc7608672e167b6216fe
Constantin <constantinu93@gmail.com>
2021-04-11 23:33:10 +0200

    feat: create utils files

---
+++ scr/utils/helpers.js
@@ -0,0 +1,8 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+//getDecks
+//getDeck
+//saveDeck
+//addCardToDeck
+
+//check Documentation of AsyncStorage for info how to setup the functions above!
