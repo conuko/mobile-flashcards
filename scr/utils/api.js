d82d1f87913fc5f8063b7c8e0fd2b4655790bd1c
Constantin <constantinu93@gmail.com>
2021-04-13 19:56:52 +0200

    feat: develop the functions for api.js

---
+++ scr/utils/api.js
@@ -0,0 +1,54 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+//getDecks: return all of the decks along with their titles, questions, and answers. (DONE)
+//getDeck:  take in a single id argument and return the deck associated with that id.
+//saveDeck: take in a single title argument and add it to the decks.
+//addCardToDeck: take in two arguments, title and card, and will add the card to the list of questions for the deck with the associated title.
+//check Documentation of AsyncStorage for info how to setup the functions above!
+
+/*
+I set this key, which is basically just where we're going to persist the
+information inside of our AsyncStore (like we did in the FitnessApp Project):
+*/
+const CARD_STORAGE_KEY = "MobileFlashcards:cards";
+
+/*
+I created the following async functions with the help of the following knowledge/resources:
+https://knowledge.udacity.com/questions/480149
+https://www.javatpoint.com/react-native-asyncstorage
+https://react-native-async-storage.github.io/async-storage/docs/api
+https://knowledge.udacity.com/questions/135724
+*/
+export const getDecks = async () => {
+  const data = await AsyncStorage.getItem(CARD_STORAGE_KEY);
+  return JSON.parse(data);
+};
+
+// take in a single id argument and return the deck associated with that id from all decks:
+export const getDeck = async (id) => {
+  getDecks().then((decks) => {
+    return decks[id];
+  });
+};
+
+export const saveDeck = async (title) => {
+  const savedDeck = JSON.stringify({
+    [title]: { title: title, questions: [] },
+  });
+  await AsyncStorage.mergeItem(CARD_STORAGE_KEY, savedDeck);
+  return JSON.parse(savedDeck);
+};
+
+/* I created the following async function with the help of the following knowledge:
+https://knowledge.udacity.com/questions/135724
+*/
+export const addCardToDeck = async (title, newCard) => {
+  const value = await AsyncStorage.getItem(CARD_STORAGE_KEY);
+  const data = JSON.parse(value);
+  const deck = data[title];
+  deck.questions.push(newCard);
+  const newDeck = { [title]: deck };
+  return await AsyncStorage.mergeItem(
+    CARD_STORAGE_KEY,
+    JSON.stringify(newDeck)
+  );
+};
--- scr/utils/helpers.js
+++
@@ -1,8 +0,0 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-
-//getDecks: return all of the decks along with their titles, questions, and answers.
-//getDeck:  take in a single id argument and return the deck associated with that id.
-//saveDeck: take in a single title argument and add it to the decks.
-//addCardToDeck: take in two arguments, title and card, and will add the card to the list of questions for the deck with the associated title.
-
-//check Documentation of AsyncStorage for info how to setup the functions above!
