9194d93bbdb61a8f908b46e835d5fa50f174f0ed
Constantin <constantinu93@gmail.com>
2021-04-15 20:11:54 +0200

    create the store in App.js

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
+import middleware from "./middleware";
+
+const store = createStore(reducer, middleware);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to Mobile Flashcards!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Welcome to Mobile Flashcards!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
