83b62272bc55fd1c39f223bb58397e483ff4aa05
Constantin <constantinu93@gmail.com>
2021-04-26 20:03:05 +0200

    fix MainNavigation

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,12 +1,9 @@
 import React from "react";
-import { TouchableOpacity } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Appbar, Avatar } from "react-native-paper";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import DeckList from "./DeckList";
 import IndividualDeck from "./IndividualDeck";
 import Quiz from "./Quiz";
-import DeckListCard from "./DeckListCard";
 import BottomTabs from "./BottomTabs";
 
 /*
@@ -19,39 +16,19 @@ https://reactnavigation.org/docs/stack-navigator
 
 const Stack = createStackNavigator();
 
-const Header = ({ scene, previous, navigation }) => {
-  const { options } = scene.descriptor;
-  const title =
-    options.headerTitle !== undefined
-      ? options.headerTitle
-      : options.title !== undefined
-      ? options.title
-      : scene.route.name;
-
-  return (
-    <Appbar.Header>
-      {previous ? <Appbar.BackAction onPress={navigation.pop} /> : undefined}
-      <Appbar.Content title={previous ? title : undefined} />
-    </Appbar.Header>
-  );
-};
-
 const MainNavigation = () => {
   return (
-    <Stack.Navigator
-      initialRouteName="Main"
-      headerMode="screen"
-      screenOptions={{
-        header: ({ scene, previous, navigation }) => (
-          <Header scene={scene} previous={previous} navigation={navigation} />
-        ),
-      }}
-    >
-      <Stack.Screen name="Main" component={BottomTabs} />
+    <Stack.Navigator initialRouteName="Main" headerMode="screen">
+      <Stack.Screen name="FLASHCARDS" component={BottomTabs} />
       <Stack.Screen
         name="Deck"
         component={IndividualDeck}
-        options={{ headerTitle: "Deck" }}
+        options={({ route }) => {
+          const { deckID } = route.params;
+          return {
+            title: `${deckID}`,
+          };
+        }}
       />
       <Stack.Screen
         name="Quiz"
--- src/components/NewDeck.js
+++ src/components/NewDeck.js
@@ -3,7 +3,15 @@ import { View, SubmitBtn } from "react-native";
 import { Button, Title, TextInput, Text } from "react-native-paper";
 import { useDispatch } from "react-redux";
 import { addDeck } from "../actions/index";
+import { saveDeck } from "../utils/api";
 import styled from "styled-components/native";
+import { CommonActions } from "@react-navigation/native";
+
+/*
+I created this component as we did in the react-native part with the AddEntry.js component
+and with the help of the following resources/knowledge:
+
+*/
 
 /* This will be a controlled component with two input forms.
 TODO:
@@ -15,14 +23,28 @@ const AddCardContainer = styled.View`
   padding: 16px;
 `;
 
-const NewDeck = () => {
+const NewDeck = (props) => {
   const [text, setText] = useState("");
   const dispatch = useDispatch();
 
+  // function to navigate back to the home screen:
+  const toHome = () => {
+    props.navigation.dispatch({
+      ...CommonActions.goBack(),
+      source: "NewDeck",
+    });
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
+    // update redux store:
     dispatch(addDeck(text));
+    // set the state of the NewDeck component back to "":
     setText("");
+    // go back to home-screen:
+    toHome();
+    // save to DB:
+    saveDeck(text);
   };
 
   return (
